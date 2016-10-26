40a29e796ae541bf02fff2eb8da46924ed527fe1
deepshikhadey <deepshikhadey@users.noreply.github.com>
2016-10-26 16:48:54 -0700

    Update server.js
    Just print hello world

--- server/server.js
+++ server/server.js
@@ -1,14 +1,15 @@
-var express = require("express");
-var request = require("request");
+#var express = require("express");
+#var request = require("request");
 
-var app = express(); // call the express as a function
+#var app = express(); // call the express as a function
 
-app.get("/hey", function(req, res) {
-        res.send("Hey There!!!");
-});
+#app.get("/hey", function(req, res) {
+#        res.send("Hey There!!!");
+#});
 
-app.get("/test", function(req, res) {
-        res.send("Testing 1 2 3!!!");
-});
+#app.get("/test", function(req, res) {
+#        res.send("Testing 1 2 3!!!");
+#});
 
-app.listen(3000);
+#app.listen(3000);
+console.log("Hello World");
