bd018e6f8964d04aca4406df5d68679ca7742d24
Deepshikha Dey (deepsdey) <deepsdey@cisco.com>
2016-10-26 14:55:46 -0700

    first commit
    Change-Id: If276a9254449e35d30e37a65ac3975844e02dd23

---
+++ server/server.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var request = require("request");
+
+var app = express(); // call the express as a function
+
+app.get("/hey", function(req, res) {
+        res.send("Hey There!!!");
+});
+
+app.get("/test", function(req, res) {
+        res.send("Testing 1 2 3!!!");
+});
+
+app.listen(3000);
