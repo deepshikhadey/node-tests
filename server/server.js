cc60cec034c55ea5e99ea88e810b0b1a2258d510
deepshikhadey <deepshikhadey@users.noreply.github.com>
2016-10-26 17:24:41 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,12 +3,3 @@ var request = require("request");
 
 var app = express(); // call the express as a function
 console.log("Hello World");
-#app.get("/hey", function(req, res) {
-#        res.send("Hey There!!!");
-#});
-
-#app.get("/test", function(req, res) {
-#        res.send("Testing 1 2 3!!!");
-#});
-
-#app.listen(3000);
