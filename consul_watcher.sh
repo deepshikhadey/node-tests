0f770b22a98fd2e0c5bc1dc5e8fb6773557731c6
deepshikhadey <deepshikhadey@users.noreply.github.com>
2016-10-28 15:28:16 -0700

    Update consul_watcher.sh

--- consul_watcher.sh
+++ consul_watcher.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 ip="$(/sbin/ip route|awk '/default/ { print $3 }')"
 echo "${ip}"
 sudo consul watch -http-addr=10.1.11.211:8301 -type=keyprefix -prefix=apps/apigateway/microservices /opt/api-gateway/service-onboarding/test_watcher'.py /opt/api-gateway/nginx_conf/conf.d/locations/ /opt/api-gateway/policy_files/
